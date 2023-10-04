71de7cd9e2c831e04b90f72c5b8979cb178ab9e8
ChenJonathan-tw <chenjonathan901210@gmail.com>
2023-10-04 15:22:04 +0800

    upload new corpus_op and file arrangement

--- src/corpus_op.py
+++ src/corpus_op.py
@@ -4,30 +4,37 @@
 import re
 import json
 
-with open("corpus_op_re.json","r",encoding="utf-8") as f:
-    corpus_op_re = json.load(f)
+# 讀取 re
+with open("corpus_op_re.json","r",encoding="utf-8") as e:
+    corpus_op_re = json.load(e)
 regexLIST = list(corpus_op_re.items())
-print(regexLIST)
 
+# 依 re 處理語料並寫入 _purged.txt 檔
 def sinica_purger(i):
-    with open('../Corpus/{}_sinica_raw.txt'.format(regexLIST[i][0]),encoding="utf-8") as f:
+    with open('../Corpus/purged/{}_sinica_purged.txt'.format(regexLIST[i][0]), 'w') as g: # 新增空的 .txt 檔，若有相同檔案會將之清空
+        pass    
+    with open('../Corpus/raw/{}_sinica_raw.txt'.format(regexLIST[i][0]),encoding="utf-8") as f: # 配合 sinica 格式將 raw 語料重新依 'more' 切分
         raw = ''.join(f.readlines())
-        purge = re.findall(r'{}'.format(regexLIST[i][1]), raw)
-    with open('../Corpus/{}_sinica_purged.txt'.format(regexLIST[i][0]),'w',encoding="utf-8") as g:
-        for j in range(len(purge)):
-            g.write(purge[j].replace("\t","").replace("\s","").replace(" ","")+"\n")
-            
+        rawLIST = raw.split('more\n')        
+    with open('../Corpus/purged/{}_sinica_purged.txt'.format(regexLIST[i][0]),'a',encoding="utf-8") as g: # 逐句依 re 取出含有標的詞彙的部分並寫入_purged.txt 檔
+        lineCount = 1
+        for j in rawLIST:  
+            purge = '\n'.join(re.findall(r'{}'.format(regexLIST[i][1]), j))
+            g.write(purge.replace("\t","").replace("\s","").replace(" ","")+"\n")            
+            print("{}. {} ==> {}".format(lineCount, j, purge)) # 此處將顯示 {句數}. {raw 語料} ==> {取出部分}
+            lineCount += 1
+        
 
 def main(i):
-    resultDICT = {"shei_status":True,
+    resultDICT = {"shei_status":True, # 方便了解執行狀態
                   "shenMe_status":True
                   }
     try:
         sinica_purger(i)
-    except:
+    except Exception as e:
         resultDICT["{}_status".format(regexLIST[i][0])] = False
-
-
+        print(f"Error occurred when processing '{regexLIST[i][0]}': {type(e).__name__}.")
+        
     return resultDICT
 
 if __name__ == "__main__":
