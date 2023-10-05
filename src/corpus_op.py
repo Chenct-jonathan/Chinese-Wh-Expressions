753db753d3fcb44208a923f18c2e956c60ac40a9
Chenct-jonathan <86584322+Chenct-jonathan@users.noreply.github.com>
2023-10-05 12:11:54 +0800

    adjust corpus_op_re (+\n) and purge again

--- src/corpus_op.py
+++ src/corpus_op.py
@@ -3,6 +3,7 @@
 
 import re
 import json
+from pprint import pprint
 
 # 讀取 re
 with open("corpus_op_re.json","r",encoding="utf-8") as e:
@@ -15,12 +16,12 @@ def sinica_purger(i):
         pass    
     with open('../Corpus/raw/{}_sinica_raw.txt'.format(regexLIST[i][0]),encoding="utf-8") as f: # 配合 sinica 格式將 raw 語料重新依 'more' 切分
         raw = ''.join(f.readlines())
-        rawLIST = raw.split('more\n')        
+        rawLIST = raw.split('more\n')
     with open('../Corpus/purged/{}_sinica_purged.txt'.format(regexLIST[i][0]),'a',encoding="utf-8") as g: # 逐句依 re 取出含有標的詞彙的部分並寫入_purged.txt 檔
         lineCount = 1
         for j in rawLIST:  
             purge = '\n'.join(re.findall(r'{}'.format(regexLIST[i][1]), j))
-            g.write(purge.replace("\t","").replace("\s","").replace(" ","")+"\n")            
+            g.write(purge.replace("\t","").replace("\s","").replace(" ","").replace("\n","")+"\n")            
             print("{}. {} ==> {}".format(lineCount, j, purge)) # 此處將顯示 {句數}. {raw 語料} ==> {取出部分}
             lineCount += 1
         
